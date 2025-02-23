e50aca0091931f93c3ca54b681d6b00f8c1d9ca7
YujeongLEE <dldbwjd24@gmail.com>
2025-02-23 15:20:10 +0900

    BST - insert

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -22,6 +22,16 @@ class Node {
         this.left = null
         }
 
+    insert(data) {
+        if(this.data > data && this.left){
+            this.left.insert(data);
+        }else if(this.data > data){
+            this.left = new Node(data)
+        }else if(this.data < data && this.right){
+            this.right.insert(data);
+        }else if(this.data < data){
+            this.right = new Node(data);
+    }
 }
 
 module.exports = Node;
